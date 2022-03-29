dbc0b20e3bc60bbddf0c6f9c5a6cb29dd70ae0bd
lisa241104 <ltakahashi23@missporters.org>
2022-03-28 22:21:10 -0400

    Made the structure

--- main.py
+++ main.py
@@ -1,1 +1,35 @@
-# This code was created by Lisa on 3/28/2022
+# This code was created by Lisa Takahashi on 3/28/2022
+
+class MineSweeper:
+    # Todo: Make 2d array for boards and tuples to store which cells have been opened
+    def __init__(self):
+        # Todo: Make variables for the width and height of the board and number of bombs
+        pass
+
+    def __str__(self):
+        # Todo: Prints the board
+        pass
+
+    def get_num_of_neighboring_bombs(self):
+        # Todo: return number of bombs eight direction
+        pass
+
+    def dig(self):
+        # Todo: dig the cell and check if it's bomb, if not, check open neighboring cells if needed
+        pass
+
+    def make_board(self):
+        # Todo: assign values to 2d array
+        pass
+
+    def start_game(self):
+        # Todo: prints board for the first time. if the chosen cell has bomb or the opened cells are not many,
+        #  create again
+        pass
+
+    def place_flag(self):
+        # Todo: put flag on a selected cell
+        pass
+
+
+new_game = MineSweeper()
