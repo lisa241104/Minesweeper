77dcd1f90d2bbe2f0676560e02033e6ffd7b8140
lisa241104 <ltakahashi23@missporters.org>
2022-04-05 15:48:30 -0400

    fixed some mistakes

--- main.py
+++ main.py
@@ -4,22 +4,22 @@ from random import randint
 
 
 class Minesweeper:
-    def __init__(self, width, height, num_of_bombs, initial_empty_cells):
+    def __init__(self, width, height, num_of_bombs, initial_opened_cells):
         # Todo: Make 2d array for boards and tuples to store which cells have been opened
         # Todo: Make variables for the width and height of the board and number of bombs
         self.width = width
         self.height = height
         self.num_of_bombs = num_of_bombs
-        self.initial_empty_cells = initial_empty_cells
+        self.initial_opened_cells = initial_opened_cells
 
         self.board = [[0 for _ in range(self.width)] for _ in range(self.height)]
         # 0 = closed, 1 = opened
         self.bombs = [[0 for _ in range(self.width)] for _ in range(self.height)]
         # 0 = empty, -1 = bomb, numbers = number of neighboring bombs
 
-    def __str__(self):
+    def display_board(self):
         # Todo: Prints the board
-        pass
+        print('□')
 
     def make_board(self):
         for _ in range(self.num_of_bombs):
@@ -32,15 +32,16 @@ class Minesweeper:
                     self.board[row][column] = -1  # change to bomb
                     break
 
-    def get_num_of_neighboring_bombs(board, row, column):
+    def get_num_of_neighboring_bombs(self, row, column):
         # Todo: return number of bombs eight direction
-        if board[row][column] == -1:
+        if self.board[row][column] == -1:
             return -1  # if it's bomb, return -1
         num = 0
         for a in range(-1, 2):
             for b in range(-1, 2):
-                if board[row + a][column + b] == -1 and row + a >= 0 and column + b >= 0:
-                    num += 1  # if index is negatice don't do this
+                if self.board[row + a][column + b] == -1 and self.height > row + a >= 0 \
+                        and self.width > column + b >= 0:
+                    num += 1  # if index is negative, doesn't do this
         return num  # check neighboring cells in 8 direction and return the number of bombs
 
     def dig(self, row, column):
@@ -56,6 +57,9 @@ class Minesweeper:
         # Todo: put flag on a selected cell
         pass
 
+    def play_game(self):
+        self.start_game()
+
 
 print('# # ### ### ###  ## # # ### ### ### ### ##  ')
 print('###  #  # # #   #   # # #   #   # # #   # # ')
@@ -65,11 +69,15 @@ print('# # ### # # ### ##  # # ### ### #   ### # # ')
 
 print('\nChoose level')
 
-level = int(input('0: Easy, 1: Medium, 2: Hard\n'))  # A user will choose level
 level_list = [[10, 8, 10, 0], [18, 14, 40, 0], [24, 20, 99, 0]]
-# width, height, num_of_bombs, initial_empty_cells
+# width, height, num_of_bombs, initial_opened_cells
 
-new_game = Minesweeper(level_list[level][0], level_list[level][1], level_list[level][2], level_list[level][3])
+while True:
+    level = int(input('0: Easy, 1: Medium, 2: Hard\n'))  # A user will choose level
+    try:
+        new_game = Minesweeper(level_list[level][0], level_list[level][1], level_list[level][2], level_list[level][3])
+        break
+    except IndexError:
+        print('Choose Valid Level')
 
-new_game.start_game()
-
+new_game.play_game()
